f8920f05f931609e6195f09e50533bad4493748e
benoit <benoxoft@hotmail.com>
2014-05-13 04:35:48 +0900

    Main

---
+++ game.py
@@ -0,0 +1,15 @@
+import pygame
+       
+def main():
+    pygame.init()
+    
+    screen = pygame.display.set_mode((800, 600))
+
+    pygame.display.set_caption('Bomberbirds')
+    pygame.mouse.set_visible(False)
+
+    #while True:
+        
+if __name__ == '__main__':
+    main()
+    
