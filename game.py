d0f1a42c57760c9a5cfe49294654fc687028b111
benoit <benoxoft@hotmail.com>
2014-05-13 09:15:03 +0900

    Main

--- game.py
+++ game.py
@@ -8,7 +8,15 @@ def main():
     pygame.display.set_caption('Bomberbirds')
     pygame.mouse.set_visible(False)
 
-    #while True:
+    continue_playing = True
+    while continue_playing:
+        for e in pygame.event.get():
+            if e.type == pygame.KEYDOWN:
+                pass
+            elif e.type == pygame.KEYUP:
+                if e.key == pygame.K_ESCAPE:
+                    continue_playing = False
+            pygame.time.delay(10)
         
 if __name__ == '__main__':
     main()
